a106a3730e1208c48b66d5d6639e30beda83c019
A-Perry-Bright <145833263+A-Perry-Bright@users.noreply.github.com>
2025-06-14 11:35:29 +0100

    Fix login page client-side error

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { LoginForm } from "@/components/auth/login-form"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import dynamic from "next/dynamic"
