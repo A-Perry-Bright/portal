f2345fbc6b7eac0d40f26bdedbd386bcbfd7c37d
A-Perry-Bright <145833263+A-Perry-Bright@users.noreply.github.com>
2025-06-14 10:20:16 +0100

    Fix hydration mismatch error in Image components

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -28,6 +28,7 @@ export default function LoginPage() {
                   width={64}
                   height={64}
                   className="w-16 h-16 object-contain"
+                  suppressHydrationWarning
                 />
               </div>
             </div>
